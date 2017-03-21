e71552e8d34e867981ca19e162e190a157a9cb73
Wangrui258 <1124979056@qq.com>
2017-03-21 08:45:30 +0800

    1 第一次上传

---
+++ app/src/main/java/android/wangrui/com/myfirstblbl/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package android.wangrui.com.myfirstblbl.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.wangrui.com.myfirstblbl.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
